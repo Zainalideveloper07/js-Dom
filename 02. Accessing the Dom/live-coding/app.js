62b24dcdf6daf809a7f60a65a307189592112a53
zainalideveloper07 <zainalideveloper07@gmail.com>
2025-03-26 20:01:35 +0500

    dom 1st lesson

--- 02. Accessing the Dom/live-coding/app.js
+++ 02. Accessing the Dom/live-coding/app.js
@@ -1,1 +1,5 @@
-console.log(document.getElementsByTagName("h1"));
+// console.log(document.getElementsByTagName("h1"));
+// console.log(document.getElementById("id-1"));
+// console.log(document.getElementsByClassName("cls"));
+// console.log(document.querySelector("#id-1"));
+console.log(document.querySelectorAll("li"));
