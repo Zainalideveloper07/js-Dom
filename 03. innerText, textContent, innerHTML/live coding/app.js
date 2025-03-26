96600e0ddfaed6619a76aaab6c2ab1d8e8f1f39c
zainalideveloper07 <zainalideveloper07@gmail.com>
2025-03-26 20:40:56 +0500

    innerhtml and innerContent

---
+++ 03. innerText, textContent, innerHTML/live coding/app.js
@@ -0,0 +1,13 @@
+
+// innerText
+// textContent
+// innerHTML
+// const p = document.querySelector("p");
+// console.log(p.innerText);
+// console.log(p.textContent);
+// console.log(p.innerHTML);
+
+
+const h1 = document.querySelector('h1');
+h1.innerHTML = "text changes";
+h1.innerHTML='<em>123</em>';
