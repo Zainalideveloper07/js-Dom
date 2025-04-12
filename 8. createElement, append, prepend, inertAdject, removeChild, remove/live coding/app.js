8cae197e79ec0b6fb82bb8113b2806e128124798
zainalideveloper07 <zainalideveloper07@gmail.com>
2025-04-12 17:41:14 +0500

    removeChild & remove

--- 8. createElement, append, prepend, inertAdject, removeChild, remove/live coding/app.js
+++ 8. createElement, append, prepend, inertAdject, removeChild, remove/live coding/app.js
@@ -8,7 +8,7 @@ h1.classList.add('greetings');
 body.appendChild(h1);
 
 // appendChild()
-// append()
+
 // prepend()
 // insertBefore()
 
@@ -33,8 +33,14 @@ firstP.insertAdjacentElement("beforebegin",i);
 firstP.insertAdjacentElement("afterbegin",i);
 firstP.insertAdjacentElement("beforeend",i);
 firstP.insertAdjacentElement("afterend",i);
-
-
+// append()
+let section=document.querySelector('section');
+section.append(i);
+section.prepend(i);
 // removeChild()
+const newList=document.querySelector('.new-list');
+const Fourth=document.querySelector(".fourth");
+newList.removeChild(Fourth);
 // remove()
+newList.remove();
 // ------------------------------------
