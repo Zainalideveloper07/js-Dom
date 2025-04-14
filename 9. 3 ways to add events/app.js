af6af570a86df39c20c1663917043638edd5d464
zainalideveloper07 <zainalideveloper07@gmail.com>
2025-04-14 15:25:53 +0500

    add eventListener

--- 9. 3 ways to add events/app.js
+++ 9. 3 ways to add events/app.js
@@ -1,5 +1,18 @@
 // ----------- BAD WAY-----------
-
+// const secondBtn = document.querySelector('.second-btn');
+// secondBtn.onclick=function (){
+//     console.log("hello world");
+}
 // ----------- GREAT WAY-----------
+// const best= document.querySelector('.best');
+// best.addEventListener('click',function(){
+//     console.log("hello world")
+
+// });
+
 
 // ----------- Event (e) Object -----------
+// const eventTEST=document.querySelector(".para");
+// eventTEST.addEventListener('Click'(e)=>{
+//     console.log(e);
+// })
