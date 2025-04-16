92dc282bbceb7998de6d3829420e832b6262a172
zainalideveloper07 <zainalideveloper07@gmail.com>
2025-04-16 18:34:23 +0500

    mouse & keybored event done

--- 10. Events In-Depth/2. Keyboard/app.js
+++ 10. Events In-Depth/2. Keyboard/app.js
@@ -1,2 +1,40 @@
 // https://www.w3schools.com/jsref/obj_keyboardevent.asp
 const input = document.querySelector("input");
+// input.addEventListener("keypress",()=>{
+//     console.log("hello world");
+// });
+
+// input.addEventListener("keydown",()=>{
+//     console.log("hello world");
+// });
+
+// input.addEventListener("keyup",()=>{
+//     console.log("hello world");
+// });
+
+
+// input.addEventListener("keypress",e=>{
+//     console.log(e.charCode);
+// });
+
+
+// input.addEventListener("keypress",e=>{
+//     console.log(e.code);
+// })
+
+// input.addEventListener("keypress",e=>{
+//     console.log(e.key);
+// })
+
+// input.addEventListener("keypress",e=>{
+//     console.log(e.ctrlKey);
+// })
+
+// input.addEventListener("keypress",e=>{
+//     console.log(e.shiftKey);
+// })
+
+
+// input.addEventListener("keypress",e=>{
+//     console.log(e.key);
+// })
